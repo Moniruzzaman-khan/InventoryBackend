545107c8afd90f66f7aca866741b376a0263e139
Moniruzzaman-khan <61977327+Moniruzzaman-khan@users.noreply.github.com>
2026-09-15 11:34:21 +0600

    Add files via upload

--- src/controllers/Purchases/PurchasesController.js
+++ src/controllers/Purchases/PurchasesController.js
@@ -1,7 +1,7 @@
 const ParentModel = require("../../models/Purchases/PurchaseModel");
 const ChildsModel = require("../../models/Purchases/PurchaseProductsModel");
 const CreateParentChildsService = require("../../services/common/CreateParentChildsService");
-const DataModel = require("../../models/Products/ProductsModel");
+const DataModel = ParentModel;
 const ListOneJoinService = require("../../services/common/ListOneJoinService");
 const DeleteParentChildsService = require("../../services/common/DeleteParentChildsService");
 
@@ -12,8 +12,8 @@ exports.CreatePurchases = async (req,res)=>{
 
 exports.PurchasesList=async (req, res) => {
     let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
-    let SearchArray=[{VatTax: SearchRgx},{Discount: SearchRgx},{OtherCost: SearchRgx},{GrandCost: SearchRgx},{ShippingCost: SearchRgx},{Note: SearchRgx},{'suppliers.Name': SearchRgx},{'suppliers.Address': SearchRgx},{'suppliers.Phone': SearchRgx},{'suppliers.Email': SearchRgx}]
-    let JoinStage= {$lookup: {from: "suppliers", localField: "SuppliersID", foreignField: "_id", as: "suppliers"}}
+    let SearchArray=[{VatTax: SearchRgx},{Discount: SearchRgx},{OtherCost: SearchRgx},{GrandTotal: SearchRgx},{ShippingCost: SearchRgx},{Note: SearchRgx},{'suppliers.Name': SearchRgx},{'suppliers.Address': SearchRgx},{'suppliers.Phone': SearchRgx},{'suppliers.Email': SearchRgx}]
+    let JoinStage= {$lookup: {from: "suppliers", localField: "SupplierID", foreignField: "_id", as: "suppliers"}}
     let Result=await ListOneJoinService(req,DataModel,SearchArray,JoinStage);
     res.status(200).json(Result)
 }
